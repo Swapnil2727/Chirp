5e625aec1339c23c254764420903cf7f48681667
Swapnil2727 <sapatel2727@gmail.com>
2026-03-02 16:12:41 +1100

    Implement buttons in Design Component

--- build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
@@ -1,35 +1,27 @@
 import com.plcoding.chirp.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.internal.Actions.with
-import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.invoke
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.gradle.kotlin.dsl.dependencies
 
 class CmpLibraryConventionPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply(plugin = libs.findPlugin("convention-kmp-library").get().get().pluginId)
-                apply(plugin = libs.findPlugin("compose-compiler").get().get().pluginId)
-                apply(plugin = libs.findPlugin("compose-multiplatform").get().get().pluginId)
+                apply("com.plcoding.convention.kmp.library")
+                apply("org.jetbrains.kotlin.plugin.compose")
+                apply("org.jetbrains.compose")
             }
 
-            extensions.configure<KotlinMultiplatformExtension> {
-                sourceSets {
-                    commonMain {
-                        dependencies {
-                            implementation(libs.findLibrary("jetbrains-compose-ui").get())
-                            implementation(libs.findLibrary("jetbrains-compose-foundation").get())
-                            implementation(libs.findLibrary("jetbrains-compose-material3").get())
-                            implementation(
-                                libs.findLibrary("jetbrains-compose-material-icons-core").get()
-                            )
-                        }
-                    }
-                }
+            dependencies {
+                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-ui").get())
+                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-foundation").get())
+                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material3").get())
+                "commonMainImplementation"(
+                    libs.findLibrary("jetbrains-compose-material-icons-core").get()
+                )
+
+                "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
             }
         }
     }
